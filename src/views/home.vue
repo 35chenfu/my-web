cd820fc2dfe73bb4874c777da37bebb48122a363
GridTrank <gggcccfff@outlook.com>
2023-12-05 15:53:53 +0800

    1

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,16 +3,16 @@
 <template>
 	<div class="page">
 		首页
+		<comEditor></comEditor>
 	</div>
 </template>
 <script setup lang="ts">
 import { ref, reactive, getCurrentInstance, onMounted, onActivated, nextTick,computed,watch } from 'vue';
-
+import comEditor from '@/components/comEditor.vue'
 </script>
 
 <style scoped lang="scss">
 	.page{
-        width: 300px;
-        height: 300px;
+      
     }
 </style>
